aa55d5485df4b474575af3fd832565a6d4647926
Matt Schmitt <MattSchmitt@matts-mbp.home>
2019-01-09 20:41:54 -0700

    changed create login

--- client/src/duplicate.js
+++ client/src/duplicate.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import axios from "axios"
-import Header from './Header'
+// import React from 'react';
+// import axios from "axios"
+// import Header from './Header'
 
+//email Regexpression is for the password so it can be any characters has .com or .net on the end
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+)
 
 const formValid = ({ formErrors, ...rest }) => {
   let valid = true
@@ -28,10 +32,14 @@ export default class Login extends React.Component {
     super(props)
 
     this.state = {
-      username: null,
+      firstName: null,
+      lastName: null,
+      email: null,
       password: null,
       formErrors: {
-        username: "",
+        firstName: "",
+        lastName: "",
+        email: "",
         password: ""
       }
     }
@@ -44,7 +52,9 @@ export default class Login extends React.Component {
     if (formValid(this.state)) {
       console.log(`
         --SUBMITTING--
-        Username: ${this.state.username}
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        Email: ${this.state.email}
         Password: ${this.state.password}
       `)
     } else {
@@ -62,10 +72,17 @@ export default class Login extends React.Component {
 
 
     switch (name) {
-      case "username":
-        formErrors.username =
-        value.length < 6
-            ? 'minimum 6 characters required' : ""
+      case "firstName":
+        formErrors.firstName =
+          value.length < 2 ? 'minimum 2 characters required' : ""
+        break;
+      case "lastName":
+        formErrors.lastName =
+          value.length < 2 ? 'minimum 2 characters required' : ""
+        break;
+      case "email":
+        formErrors.email =
+          emailRegex.test(value) ? '' : "Invalid email address"
         break;
       case "password":
         formErrors.password =
@@ -91,23 +108,50 @@ export default class Login extends React.Component {
         <div className="form-wrapper">
           <h1>Create Account</h1>
           <form onSubmit={this.handleSubmit} noValidate>
-           
-            <div className="username">
-              <label htmlFor="username"></label>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
               <input
-                className={formErrors.username.length > 0 ? "error" : null}
-                placeholder="Username"
-                type="username"
-                name="username"
+                className={formErrors.firstName.length > 0 ? "error" : null}
+                placeholder="First Name"
+                type="text"
+                name="firstName"
                 noValidate
                 onChange={this.handleChange}
               />
-              {formErrors.username.length > 0 && (
-                <span className="errorMessage">{formErrors.username}</span>
+              {formErrors.firstName.length > 0 && (
+                <span className="errorMessage">{formErrors.firstName}</span>
+              )}
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                className={formErrors.lastName.length > 0 ? "error" : null}
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.lastName.length > 0 && (
+                <span className="errorMessage">{formErrors.lastName}</span>
+              )}
+            </div>
+            <div className="email">
+              <label htmlFor="email">email</label>
+              <input
+                className={formErrors.email.length > 0 ? "error" : null}
+                placeholder="email"
+                type="email"
+                name="email"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.email.length > 0 && (
+                <span className="errorMessage">{formErrors.email}</span>
               )}
             </div>
             <div className="password">
-              <label htmlFor="password"></label>
+              <label htmlFor="password">Password</label>
               <input
                 className={formErrors.password.length > 0 ? "error" : null}
                 placeholder="Password"
