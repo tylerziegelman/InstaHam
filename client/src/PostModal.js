baab972520e4c7f83dacf7ff919e4ddf11a4ad4b
tylerziegelman <tylerziegelman+git@gmail.com>
2019-01-19 14:30:49 -0700

    added imageUpload to s3

--- client/src/PostModal.js
+++ client/src/PostModal.js
@@ -2,6 +2,9 @@ import { Modal, Button } from 'antd';
 import { Icon } from 'antd';
 import React from 'react'
 import './Header.css'
+import UploadButton from './UploadButton' 
+import axios from 'axios'
+import { defaultCipherList } from 'constants';
 
 class PostModal extends React.Component {
   state = { visible: false }
@@ -14,6 +17,7 @@ class PostModal extends React.Component {
 
   handleOk = (e) => {
     console.log(e);
+
     this.setState({
       visible: false,
     });
@@ -26,27 +30,58 @@ class PostModal extends React.Component {
     });
   }
 
-  render() {
-    return (
-      <div>
-         <Button type="default" className="post-button" onClick={this.showModal}>
-          Post
-        </Button>
-       
-        <Modal
-          title="Upload Your Meat"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-            <Button>
-            {/* place to tie in post logic*/}
-                <Icon type="upload" /> Upload
-            </Button>
-            <textarea className="post-textarea" placeholder="Description of image"></textarea>
-            </Modal>
-      </div>
-    );
+  handleSubmitPost = () => {
+    axios.post('/post', {
+      url: "",
+      description:""
+    },{
+      headers: {
+        Authorization: localStorage.get('instaham-jwt')
+      }
+    })
+    
+    this.setState({
+      visible: false,
+    });
   }
+
+  footer = () => {
+    return <>
+    <Button onClick= {this.handleCancel}>
+      Cancel
+    </Button>
+    <Button onClick= {this.handleSubmitPost}>
+      Post
+    </Button>
+    
+    </>
+  }
+
+render() {
+  return (
+    <div>
+      <Button type="default" className="post-button" onClick={this.showModal}>
+        Post
+        </Button>
+
+      <Modal
+        title="Upload Your Meat"
+        visible={this.state.visible}
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+        footer={this.footer()}
+      >
+        {/* <Button>
+          {/* place to tie in post logic*/}
+          {/* <Icon type="upload" /> Upload
+            </Button> */} 
+
+<UploadButton/>
+
+        <textarea className="post-textarea" placeholder="Description of image"></textarea>
+      </Modal>
+    </div>
+  );
+}
 }
 export default PostModal
