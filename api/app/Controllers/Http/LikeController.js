afc0cc17384c371bf9b5ca545f6bb9866d5b9619
Matt Schmitt <mw.schmitt2@gmail.com>
2019-01-20 14:51:21 -0700

    like dislike only once

--- api/app/Controllers/Http/LikeController.js
+++ api/app/Controllers/Http/LikeController.js
@@ -2,17 +2,28 @@
 const Like=use('App/Models/Like')
 const User=use('App/Models/User')
 const Post=use('App/Models/Post')
-
+const Database=use('Database')
 class LikeController {
 
     async createLike ({request, response,auth}) {
         const user = await auth.getUser()
         const {post_id,type} = request.post()
-        const like = await Like.create({user_id:user.id,post_id,type:type})
-        response.json({
-            message:`Like was created`,
-            data: like
-        })
+        const likeChecker = await Database.from('likes').where('user_id', user.id).where('post_id', post_id)
+        
+        
+         if(likeChecker.length){
+             response.json({
+                 message: 'already liked by this user'
+             })
+         }else {
+            const like = await Like.create({user_id:user.id,post_id,type:type})
+            response.json({
+                message:`Like was created`,
+                data: like
+            })
+         }
+
+        
     }
 }
 
